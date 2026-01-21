488c1dcbe3db2cefd744aef96dfed547b8432dae
Deepak9551 <12pcm.deepak@gmail.com>
2026-01-21 10:37:20 +0530

    features

--- src/main/resources/db/migration/V3__some_modification.sql
+++ src/main/resources/db/migration/V3__some_modification.sql
@@ -1,7 +1,7 @@
-ALTER TABLE mosh2.cart_items DROP FOREIGN KEY fk_cart;
+ALTER TABLE cart_items DROP FOREIGN KEY fk_cart;
 
 -- Parent table change karein
-ALTER TABLE mosh2.carts MODIFY id BINARY(16) NOT NULL;
+ALTER TABLE carts MODIFY id BINARY(16) NOT NULL;
 
 -- Child table ka reference column bhi change karein
-ALTER TABLE mosh2.cart_items MODIFY cart_id BINARY(16) NOT NULL;
+ALTER TABLE cart_items MODIFY cart_id BINARY(16) NOT NULL;
